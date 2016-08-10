e28c864f8fc35d3c9d0093e230bb72152ad421d4
Michael Quaas <michael.quaas@betterplace.org>
2016-08-10 12:51:40 +0200

    I18n setup

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,9 @@ module EventManagement
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+    config.i18n.default_locale = :de
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
