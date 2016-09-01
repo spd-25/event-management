8f6361d220703d77071a71e3a1c9102f7f6885d2
Michael Quaas <michael.quaas@betterplace.org>
2016-09-01 13:44:41 +0200

    search helper: fix icons

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,10 +1,10 @@
 module SearchHelper
   def display_name_for(result)
     case result
-    when Seminar then [seminars_icon(result.number), result.title]
-    when Teacher then [teachers_icon(result.name)]
-    when Location then [locations_icon(result.name), result.description]
-    when Category then [categories_icon(result.name)]
+    when Seminar then [icon_for(result, text: result.number), result.title]
+    when Teacher then [icon_for(result, text: result.name)]
+    when Location then [icon_for(result, text: result.name), result.description]
+    when Category then [icon_for(result, text: result.name)]
     end.select(&:present?).join(', ').html_safe
   end
 end
