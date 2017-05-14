a30718591f29d3ec5504b0c446dfceacbdade18b
Michael Quaas <michael.quaas@betterplace.org>
2017-05-14 20:38:12 +0200

    specs: create catalog before all

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,6 +22,12 @@ SimpleCov.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  config.before(:all) do
+    Catalog.find_or_create_by! title: 'Catalog', year: Date.current.year
+  end
+
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
