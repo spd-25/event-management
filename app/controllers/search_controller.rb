8dd79b7e05f823b6ab4b5c27780d182ec1db830d
Michael Quaas <michael@qua.as>
2017-08-04 11:34:46 +0200

    Fix attendee search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
     authorize :search
     @query = params[:q]
     @result = PgSearch.multisearch(@query).includes(:searchable).to_a.map(&:searchable)
-    @result = @result.select { |s| s.respond_to?(:year) ? s.year == current_year : true }
+    @result = @result.select { |s| s.is_a?(Attendee) || (s.respond_to?(:year) ? s.year == current_year : true) }
     if @result.count == 1
       one_hit = @result.first
       redirect_to one_hit.is_a?(Attendee) ? one_hit.booking.seminar : one_hit
