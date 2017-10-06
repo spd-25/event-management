92f2e56e59b5d852528eba6c95e4b4b467c34db9
Michael Quaas <michael@qua.as>
2017-10-06 18:31:43 +0200

    Fix feedbacks factory

--- spec/factories/feedbacks.rb
+++ spec/factories/feedbacks.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :feedback do
-    subject "MyString"
-    message "MyText"
-    path "MyString"
-    user nil
+    subject 'Problem'
+    message 'Something is not working'
+    path 'https://server.com/path'
+    user { create :user, name: 'feedbacker' }
   end
 end
