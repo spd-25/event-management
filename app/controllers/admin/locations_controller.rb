f7031f7efb04d4e3e2dda2b40adb2e03ad7c6e9a
Michael Quaas <michael@qua.as>
2018-01-19 17:32:36 +0100

    moving location part to

--- app/controllers/admin/locations_controller.rb
+++ app/controllers/admin/locations_controller.rb
@@ -1,55 +1,55 @@
-class LocationsController < ApplicationController
-  before_action :authenticate_user!
-  after_action :verify_authorized
-  before_action :set_location, only: [:show, :update, :destroy]
+module Admin
+  class LocationsController < BaseController
+    before_action :set_location, only: %i[show update destroy]
 
-  def index
-    authorize Location
-    @locations = Location.order(:name).all
-  end
-
-  def new
-    authorize Location
-    @location = Location.new
-  end
-
-  def show
-  end
-
-  def create
-    authorize Location
-    @location = Location.new location_params
-
-    if @location.save
-      redirect_to locations_url, notice: t(:created, model: Location.model_name.human)
-    else
-      render :new
+    def index
+      authorize Location
+      @locations = Location.order(:name).all
     end
-  end
 
-  def update
-    if @location.update location_params
-      redirect_to locations_url, notice: t(:updated, model: Location.model_name.human)
-    else
-      render :show
+    def new
+      authorize Location
+      @location = Location.new
     end
-  end
 
-  def destroy
-    @location.destroy
-    redirect_to locations_url, notice: t(:destroyed, model: Location.model_name.human)
-  end
+    def show
+    end
 
-  private
+    def create
+      authorize Location
+      @location = Location.new location_params
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_location
-    @location = Location.find params[:id]
-    authorize @location
-  end
+      if @location.save
+        redirect_to admin_locations_url, notice: t(:created, model: Location.model_name.human)
+      else
+        render :new
+      end
+    end
 
-  # Only allow a trusted parameter "white list" through.
-  def location_params
-    params.require(:location).permit(:name, :description, address: %i(street zip city))
+    def update
+      if @location.update location_params
+        redirect_to admin_locations_url, notice: t(:updated, model: Location.model_name.human)
+      else
+        render :show
+      end
+    end
+
+    def destroy
+      @location.destroy
+      redirect_to admin_locations_url, notice: t(:destroyed, model: Location.model_name.human)
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_location
+      @location = Location.find params[:id]
+      authorize @location
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def location_params
+      params.require(:location).permit(:name, :description, address: %i[street zip city])
+    end
   end
 end
