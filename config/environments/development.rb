6712c77df0d31380cc052d8b874febb51e726a86
Michael Quaas <michael@qua.as>
2018-03-17 14:36:38 +0100

    Alchemy contact form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,11 +52,11 @@ Rails.application.configure do
     password:             Setting.email_password
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
 
   # Suppress logger output for asset requests.
