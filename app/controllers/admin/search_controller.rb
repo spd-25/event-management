9d77a16861fcfbb91bea2dabfad1dce71a0c241b
Michael Quaas <michael@qua.as>
2018-03-22 14:18:36 +0100

    fix urls (main_app)

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -7,7 +7,8 @@ module Admin
       @result = @result.select { |s| s.is_a?(Attendee) || (s.respond_to?(:year) ? s.year == current_year : true) }
       return unless @result.count == 1
       one_hit = @result.first
-      redirect_to one_hit.is_a?(Attendee) ? admin_seminar_url(one_hit.booking.seminar) : one_hit
+      one_hit = one_hit.booking.seminar if one_hit.is_a?(Attendee)
+      redirect_to [:admin, one_hit]
     end
   end
 end
